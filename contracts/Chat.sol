e1eb3b9a1250bded41803d269f5a8a255f5cdebb
Simon Sauperl <s.sauperl@netconomy.net>
2019-09-09 11:09:36 +0200

    work on smart contract

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -3,31 +3,30 @@ pragma solidity >=0.4.0 <0.6.0;
 
 contract Chat {
 
-
-struct fromToKey {
-
-  address fromAddress;
-
-  address toAddress; 
-
- }
- 
- struct message {
+ struct  Message {
+     address origin;
      string timestamp;
-     string text:
+     string text;
  }
  
-//save own messages here, encripted for sender
-mapping (fromToKey => message) public sendMessages;
+struct UserChat {
+  address owner;
+  mapping (address => Message[]) messages ;
+ }
+ 
 
-//save target messages, encripted for reciever
-mapping (fromToKey => message) public recievedMessages;
+ 
+mapping (address => UserChat) public  chats;
 
 
-function sendAMessage(string toAddress), string messege) public {
 
-    msg.sender
-     
+function sendAMessage(address toAddress, string memory content, string memory timestamp) public {
+    UserChat storage senderChat = chats[msg.sender];
+    Message memory userMessage = Message(msg.sender, content, timestamp);
+    senderChat.messages[toAddress].push(userMessage);
+    
+    UserChat storage recieverChat = chats[toAddress];
+    recieverChat.messages[toAddress].push(userMessage);
 
 }
 
